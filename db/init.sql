9af90e9d35395b47f2e28b77d38259316f44e580
Carla Prado <ca.pradom@duocuc.cl>
2026-05-11 19:42:27 -0400

    churus

--- db/init.sql
+++ db/init.sql
@@ -14,3 +14,4 @@ INSERT INTO productos (nombre, descripcion, precio, stock) VALUES
 ('Alimento Adulto Light', 'Control de peso, razas medianas', 17990, 8),
 ('Snacks Dentales', 'Ayuda a la limpieza dental', 5990, 30);
 ('Snack Premium', 'Sabor Vacuno', 8990, 20);
+('Churus gato ninja', 'Todos los sabores', 60000, 100);
